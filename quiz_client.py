e1658a8c8ecb7092f45f366283ac78b452e03428
shrihari237 <90607160+shrihari237@users.noreply.github.com>
2024-02-16 14:42:51 +0530

    Update quiz_client.py

--- quiz_client.py
+++ quiz_client.py
@@ -8,27 +8,30 @@ from tkinter import *
 class GUI:
     def __init__(self):
         self.Window = Tk()
+        self.Window.title("Quiz App")
         self.Window.withdraw()
 
         self.login = Toplevel()
         self.login.title = "Login"
         self.login.resizable(width=False,height=False)
-        self.login.configure(width=400,height=300,bg="cyan")
+        self.login.configure(width=400,height=300)
 
-        self.pls = Label(self.login,font="Helvetica 14 bold",justify=CENTER,text="Please login to continue!",fg ="Black",bg="lightcyan")
+        self.pls = Label(self.login,font="Helvetica 14 bold",justify=CENTER,text="Please login to continue!",fg ="Black")
         self.pls.place(relheight=0.15,relx=0.2,rely=0.07)
 
-        self.name_label  = Label(self.login,font="Helvetica 14 bold",text="Enter your username:",fg ="Black",bg="lightcyan")
+        self.name_label  = Label(self.login,font="Helvetica 14 bold",text="Enter your username:",fg ="Black")
         self.name_label.place(relheight=0.2,relx=0.1,rely=0.2)
 
-        self.entry_name = Entry(self.login,font="Helvetica 14 bold",text="")
+        self.entry_name = Entry(self.login,font="Helvetica 14 bold",text="",bg = "lightcyan")
         self.entry_name.place(relwidth=0.4,relheight=0.12,relx=0.35,rely=0.4)
         self.entry_name.focus()
 
         self.button = Button(self.login,
                              font="Helvetica 14 bold",
                              text="CONTINUE",
-                             command=lambda : self.goahead(self.entry_name.get()))
+                             command=lambda : self.goahead(self.entry_name.get()),
+                             bg="cyan")
+        self.button.place(relx=0.4,rely=0.55)
     
 
         self.Window.mainloop()
